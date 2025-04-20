5c1c16cf949dadc461d24b120d56f675ef93175f
manish singh <91075206+msrajpoot1000@users.noreply.github.com>
2025-04-20 20:57:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* Public Routes */}
+      <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
